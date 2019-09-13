a318bfaa2153a3939550aef3661ece99e40d2114
jimmyvietnam <53327087+jimmyvietnam@users.noreply.github.com>
2019-09-13 23:29:53 +0700

    Create footer-new.php

---
+++ footer-new.php
@@ -0,0 +1,39 @@
+		</div>
+        <footer class="footer-new">
+            <div class="container-md">
+                <div class="footer-new-links layout-gt-sm-row layout-align-space-between-center">
+                    <a href="/">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/resources/images/transform-white-logo.svg">
+                    </a>
+                    <div>
+                        <a href="https://support.transform.vn" target="_blank">Hỗ trợ</a>
+                        <a href="#" target="_blank">Gởi yêu cầu tư vấn</a>
+                        <a href="/blog" target="_blank">Blog</a>
+                        <a href="#" target="_blank">Giới thiệu Transform.vn</a>
+                        <a href="/sitemap">Liên lạc</a>
+                    </div>
+                </div>
+                <div class="footer-new-legal layout-gt-sm-row">
+                    <div class="footer-new-legal-copy flex-gt-sm">
+                        <p class="footer-new-copyright"><?php do_action( 'twentytwelve_credits' ); ?></p>
+                        <p class="footer-new-trademarks">Transform®, Digital Transformation®, Chuyển đổi số™, Ứng dụng công nghệ®, CRM®, Customer Experiences® are registered trademarks of Transform Asia.</p>
+                    </div>
+                    <div class="built-mn flex-gt-sm-none">
+                        <div class="built-mn-graphic"></div>
+                        <div class="built-mn-text">
+                            Make by
+                            <br />Transform
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+    <script type="text/javascript">
+        /*(function (e, t, o, n, p, r, i) { e.visitorGlobalObjectAlias = n; e[e.visitorGlobalObjectAlias] = e[e.visitorGlobalObjectAlias] || function () { (e[e.visitorGlobalObjectAlias].q = e[e.visitorGlobalObjectAlias].q || []).push(arguments) }; e[e.visitorGlobalObjectAlias].l = (new Date).getTime(); r = t.createElement("script"); r.src = o; r.async = true; i = t.getElementsByTagName("script")[0]; i.parentNode.insertBefore(r, i) })(window, document, "https://diffuser-cdn.app-us1.com/diffuser/diffuser.js", "vgo");
+        vgo('setAccount', '66267832');
+        vgo('setTrackByDefault', true);*/
+    </script>
+	<?php wp_footer(); ?>
+</body>
+</html>
