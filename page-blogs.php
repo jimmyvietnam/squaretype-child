c3402a8eaedb1358866a90fba2a1955fcb68b5c4
jimmyvietnam <53327087+jimmyvietnam@users.noreply.github.com>
2019-09-14 19:11:04 +0700

    Update page-blogs.php

--- page-blogs.php
+++ page-blogs.php
@@ -1,18 +1,6 @@
 <?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link http://codex.wordpress.org/Template_Hierarchy
- *
- * @package Squaretype
- */
-
-get_header(); ?>
+get_header(); 
+?>
 
 	<div id="primary" class="content-area">
 
@@ -24,6 +12,8 @@ get_header(); ?>
 
 			do_action( 'csco_main_start' );
 
+			query_posts('post_type=post&post_status=publish&posts_per_page=10&paged='. get_query_var('paged'));
+			
 			if ( have_posts() ) {
 				$archive_layout = get_theme_mod( csco_get_archive_option( 'layout' ), 'list' );
 
@@ -91,6 +81,8 @@ get_header(); ?>
 				<?php
 			}
 
+			wp_reset_query();
+			
 			do_action( 'csco_main_end' );
 			?>
 
